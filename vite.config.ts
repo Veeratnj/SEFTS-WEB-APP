95b75564d6d8678fcfe75aa2bf3d46e57312268c
veeratnj <veerakumarhgkfm@gmail.com>
2025-05-23 11:14:22 +0530

    vite config added --commit

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),tailwindcss()],
   server: {
+    host: '0.0.0.0',
     allowedHosts: ['smartelitetradingclub.com', 'www.smartelitetradingclub.com']
   }
   
