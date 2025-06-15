287869d5570f81c7d6c3023c0b79522a56852c7c
Veeratnj <veerakumarhgkfm@gmail.com>
2025-06-15 14:12:50 +0530

    logout cookies issue test --commit

--- src/components/HeaderComponents.tsx
+++ src/components/HeaderComponents.tsx
@@ -29,6 +29,11 @@ const HeaderComponents: React.FC = () => {
   const handleLogout = () => {
     localStorage.removeItem('userData');
     localStorage.setItem('isAuthenticated', 'false');
+    document.cookie.split(";").forEach(cookie => {
+    const name = cookie.split("=")[0].trim();
+    document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/';
+  });
+
     window.location.href = '/login';
   };
 
